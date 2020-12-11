b291ca9789adc3e900dfd0eb3f6b9d1da5c19f95
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-12-11 23:32:06 +0200

    Initial Commit

---
+++ AsyncImageDownloadUsingCombineAndSwiftUI/AsyncImageDownloadUsingCombineAndSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageDownloadUsingCombineAndSwiftUIApp.swift
+//  AsyncImageDownloadUsingCombineAndSwiftUI
+//
+//  Created by Ramill Ibragimov on 11.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageDownloadUsingCombineAndSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImageDownloadUsingCombineAndSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncImageDownloadUsingCombineAndSwiftUI
+//
+//  Created by Ramill Ibragimov on 11.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
