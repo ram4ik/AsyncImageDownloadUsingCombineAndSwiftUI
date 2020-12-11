ed980c197580ce39ad04700dd4eeba6fbb380d45
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-12-11 23:58:58 +0200

    init

--- AsyncImageDownloadUsingCombineAndSwiftUI/ContentView.swift
+++ AsyncImageDownloadUsingCombineAndSwiftUI/ContentView.swift
@@ -6,11 +6,77 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    let urls = [
+        "https://www.citroen.ee/files/1003533.jpg",
+        "https://www.citroen.ee/files/1003427.jpg",
+        "https://www.citroen.ee/files/1003422.jpg",
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            ForEach(urls, id: \.self) { url in
+                AsyncImageView(url: URL(string: url)!)
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+    }
+}
+
+struct AsyncImageView: View {
+    @ObservedObject private var downloader: ImageDownloader
+    
+    private var image: some View {
+        Group {
+            if downloader.image != nil {
+                Image(uiImage: downloader.image!).resizable()
+            } else {
+                ProgressView()
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+    }
+    
+    init(url: URL) {
+        downloader = ImageDownloader(url: url)
+    }
+    
+    var body: some View {
+        image
+            .onAppear() {
+                downloader.start()
+            }
+            .onDisappear() {
+                downloader.stop()
+            }
+    }
+}
+
+class ImageDownloader: ObservableObject {
+    @Published private(set) var image: UIImage?
+    private let url: URL
+    private var cancellable: AnyCancellable?
+    
+    init(url: URL) {
+        self.url = url
+    }
+    
+    func start() {
+        cancellable = URLSession(configuration: .default)
+            .dataTaskPublisher(for: url)
+            .map { UIImage(data:  $0.data) }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.image, on: self)
+    }
+    
+    func stop() {
+        cancellable?.cancel()
+    }
+    
+    deinit {
+        cancellable?.cancel()
     }
 }
 
